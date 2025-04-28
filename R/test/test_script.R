c567fae504edd4ca228458f41a75b5f55978fe14
Viera Gnahore <hln526@york.ac.uk>
2025-04-28 03:26:23 +0100

    Passed 1/3 tests, very confused about the Add function and the Remove function. I have asked the internet and my peers and this is the best I could do

--- R/test/test_script.R
+++ R/test/test_script.R
@@ -1,5 +1,5 @@
 suppressPackageStartupMessages({
-library(testthat)
+  library(testthat)
 })
 
 # Run like:
@@ -53,72 +53,70 @@ test_that("remove_task works correctly", {
   file.remove(temp_list)
 })
 
-##test_that("add_task works correctly", {
+test_that("add_task works correctly", {
   # Assuming test_file is defined and the necessary setup is done
-
-#  temp_list <- ".temp_test.txt"
-#  file.copy(test_file, temp_list, overwrite=TRUE)
-# TASK_FILE <<- temp_list
-
-#  add_task("Item 6")
-
- # task_list <- list_tasks()
-  #expected_list <- "1. Item 1\n2. Item 2\n3. Item 3\n4. Item 4\n5. Item 5\n6. Item 6"
-
- # expect_equal(task_list, expected_list)
-
- # file.remove(temp_list)
-#})
-
-#test_that("check main", {
-
-#  temp_list <- ".temp_test.txt"
-#  file.copy(test_file, temp_list, overwrite=TRUE)
-  #TASK_FILE <<- temp_list
-
-#  # parse_args sets all arguments, so we need to create them all
-#  args <- c("list" = TRUE, "add" = NULL, "remove" = NULL)
-#  args <- data.frame(t(args))
-#  output <- capture.output(main(args))
-#  expect_equal(nchar(output), 59)
-
-#  args <- c("add" = "Item 6", "list" = FALSE, "remove" = NULL)
- # args <- data.frame(t(args))
-#  output <- capture.output(main(args))
-#  expect_identical(output, character(0))
-
- # args <- c("list" = TRUE, "add" = NULL, "remove" = NULL)
- # args <- data.frame(t(args))
- # output <- capture.output(main(args))
- # expect_equal(nchar(output), 70)
-#  expected_list <- "[1] \"1. Item 1\\n2. Item 2\\n3. Item 3\\n4. Item 4\\n5. Item 5\\n6. Item 6\""
- # expect_equal(output, expected_list)
-
- # args <- c("remove" = 6, "add" = NULL, "list" = FALSE)
- # args <- data.frame(t(args))
-  #output <- capture.output(main(args))
- # expect_lt(nchar(output), 25)
-
   
- # file.remove(temp_list)
+  temp_list <- ".temp_test.txt"
+  file.copy(test_file, temp_list, overwrite=TRUE)
+  TASK_FILE <<- temp_list
+  
+  add_task("Item 6")
+  
+  task_list <- list_tasks()
+  expected_list <- "1. Item 1\n2. Item 2\n3. Item 3\n4. Item 4\n5. Item 5\n6. Item 6"
+  
+  expect_equal(task_list, expected_list)
+  
+  file.remove(temp_list)
+})
 
-#})
+test_that("check main", {
+  
+  temp_list <- ".temp_test.txt"
+  file.copy(test_file, temp_list, overwrite=TRUE)
+  TASK_FILE <<- temp_list
+  
+  # parse_args sets all arguments, so we need to create them all
+  args <- c("list" = TRUE, "add" = NULL, "remove" = NULL)
+  args <- data.frame(t(args))
+  output <- capture.output(main(args))
+  expect_equal(nchar(output), 59)
+  
+  args <- c("add" = "Item 6", "list" = FALSE, "remove" = NULL)
+  args <- data.frame(t(args))
+  output <- capture.output(main(args))
+  expect_identical(output, character(0))
+  
+  args <- c("list" = TRUE, "add" = NULL, "remove" = NULL)
+  args <- data.frame(t(args))
+  output <- capture.output(main(args))
+  expect_equal(nchar(output), 70)
+  expected_list <- "[1] \"1. Item 1\\n2. Item 2\\n3. Item 3\\n4. Item 4\\n5. Item 5\\n6. Item 6\""
+  expect_equal(output, expected_list)
+  
+  args <- c("remove" = 6, "add" = NULL, "list" = FALSE)
+  args <- data.frame(t(args))
+  output <- capture.output(main(args))
+  expect_lt(nchar(output), 25)
+  
+  
+  file.remove(temp_list)
+  
+})
 
 
-#if (requireNamespace("lintr")) {
-#library(lintr)
-
-#context("linting script")
-#test_that("Coding style", {
-  #output<-lintr::lint("../todo.R")
-  #expect_lt(length(output),500)
-  #expect_lt(length(output),400)
-  #expect_lt(length(output),250)
-  #expect_lt(length(output),100)
-  #expect_lt(length(output),50)
-  #expect_lt(length(output),10)
-  #expect_equal(length(output),0)
-#})
-#}
-
-
+if (requireNamespace("lintr")) {
+  library(lintr)
+  
+  context("linting script")
+  test_that("Coding style", {
+    output<-lintr::lint("../todo.R")
+    expect_lt(length(output),500)
+    expect_lt(length(output),400)
+    expect_lt(length(output),250)
+    expect_lt(length(output),100)
+    expect_lt(length(output),50)
+    expect_lt(length(output),10)
+    expect_equal(length(output),0)
+  })
+}
--- R/todo.R
+++ R/todo.R
@@ -5,11 +5,25 @@ suppressPackageStartupMessages({
 
 TASK_FILE <- "test_list.txt" # nolint
 
-add_task <- function(task) {
-
+add_task <- function(task) { # this function does not work, not sure why
+  tasks <- readLines(TASK_FILE)
+  new_task <- readLines(file("stdin"),1)
+  counter <- 1
+  output_string <- ""
+  for (task in tasks) {
+    if (counter < 5) { 
+      output_string <- paste0(output_string, counter, ". ", task, "\n")
+      counter <- counter + 1
+      return(output_string)
+    }
+    else {
+      output_string <- paste0(output_string, counter, ". ", task, new_task) 
+  }
+  return(output_string)
+}
 }
 
-list_tasks <- function() {
+list_tasks <- function() { #this function works
   tasks <- readLines(TASK_FILE)
   counter <- 1
   output_string <- ""
@@ -26,12 +40,14 @@ list_tasks <- function() {
 }
 
 
-remove_task <- function(index) {
+remove_task <- function(index) { #Just can't figure out/find the right functions
   tasks <- readLines(TASK_FILE)
   task_removed <- readLines(file("stdin"),1)
-  for (task in tasks) {
+ for (task in tasks) {
     if (task_removed==task) {
-      print(tasks)
+      task <- NULL
+      #a function that loops through the list, looks at the index of the matching text, 
+      #and removes it without disrupting list_task()
     }
     }
   }
@@ -52,7 +68,7 @@ main <- function(args) {
 }
 
 
-if (sys.nframe() == 0) {
+if (sys.nframe() == 0) { #very confused on how to use these in my functions
   
   # main program, called via Rscript
   parser <- ArgumentParser(description = "Command-line Todo List")
@@ -65,5 +81,5 @@ if (sys.nframe() == 0) {
                       help = "Remove a task by index")
   
   args <- parser$parse_args()
-  main(args)
+  main(args) 
 }
